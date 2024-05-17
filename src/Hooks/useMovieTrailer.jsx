b5357485def16d58ad209d9043ef53bb3d779abf
Omkesh Jadhav <iamomkeshjadhav@gmail.com>
2024-05-17 13:42:39 +0530

    embedded youtube video

---
+++ src/Hooks/useMovieTrailer.jsx
@@ -0,0 +1,32 @@
+import { useDispatch } from "react-redux";
+import { API_OPTIONS } from "../utils/constants";
+import { addTrailerVideo } from "../data/movieSlice";
+import { useEffect } from "react";
+
+const useMovieTrailer = (movieId) => {
+  const dispatch = useDispatch();
+
+  const getMovieVideo = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/" +
+        movieId +
+        "/videos?language=en-US",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    // console.log(json);
+
+    const filterData = json.results.filter(
+      (video) => video.name === "Official Trailer"
+    );
+    const trailer = filterData.length ? filterData[0] : json.results[0]; // condition for if offical trailer is not available
+    dispatch(addTrailerVideo(trailer));
+    // console.log(trailer.key);
+  };
+
+  useEffect(() => {
+    getMovieVideo();
+  }, []);
+};
+
+export default useMovieTrailer;
