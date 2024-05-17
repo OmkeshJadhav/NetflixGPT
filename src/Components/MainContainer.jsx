e1db71a91f8b83a546c5471a04a6b0ab638a2dba
Omkesh Jadhav <iamomkeshjadhav@gmail.com>
2024-05-17 13:43:10 +0530

    Styling added for main container

---
+++ src/Components/MainContainer.jsx
@@ -0,0 +1,26 @@
+import { useSelector } from "react-redux";
+import VideoBackground from "./VideoBackground";
+import VideoTitle from "./VideoTitle";
+
+const MainContainer = () => {
+  const movies = useSelector((store) => store.movies?.nowPlayingMovies);
+  // console.log(movies);
+
+  if (!movies || movies.length === 0) return null; // If nowPlayingMovies has no movie then return ---> Early return Saves the code from failing
+
+  const mainMovie = movies[0];
+  // console.log(mainMovie);
+
+  if (!mainMovie) return null;
+
+  const { original_title, overview, id } = mainMovie;
+
+  return (
+    <div className="pt-[30%] md:pt-0">
+      <VideoTitle title={original_title} overview={overview} />
+      <VideoBackground movieId={id} />
+    </div>
+  );
+};
+
+export default MainContainer;
