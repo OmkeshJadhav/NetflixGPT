234bbf59fb8cbab44be8ae9dfc9aebbd7ff4f6ce
Omkesh Jadhav <iamomkeshjadhav@gmail.com>
2024-05-21 13:47:44 +0530

    Dummy Key for OpenAI

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -17,4 +17,5 @@ export const API_OPTIONS = {
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500/";
 
-export const OPENAI_KEY = process.env.REACT_APP_OPENAI;
+export const OPENAI_KEY =
+  "sk-proj-9BssxhoSR0I25AUYUBwKT3BlbkFJviH0lQap0bbwwuzK00ST";
