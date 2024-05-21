2dfdb8cfa469bb5afd9309b9d801660e2284fea1
Omkesh Jadhav <iamomkeshjadhav@gmail.com>
2024-05-21 13:33:37 +0530

    openai

--- src/utils/openai.jsx
+++ src/utils/openai.jsx
@@ -1,7 +1,8 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: process.env.REACT_APP_OPENAI,
+  apiKey:
+    process.env["sk-proj-9BssxhoSR0I25AUYUBwKT3BlbkFJviH0lQap0bbwwuzK00ST"],
   dangerouslyAllowBrowser: true,
 });
 
